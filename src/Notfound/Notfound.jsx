0971e7ed4ec04664442349981efa1897b4304067
marwanahmed-123 <7marawan7@gmail.com>
2025-02-18 12:37:12 +0200

    third commit

--- src/Notfound/Notfound.jsx
+++ src/Notfound/Notfound.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
 import Style from "./Notfound.module.css";
+import myImage from "../assets/error.svg";
 export default function Notfound() {
-  const [testString, setTestString] = useState("test");
-  useEffect(() => {}, []);
   return (
-    <div>
-      <h2 className={`${Style["bg-reeeed"]}`}>Notfound compo </h2>
-      <p>{testString} Lorem, ipsum dolor.</p>
-    </div>
+    <>
+      <img src={myImage} alt="" className="w-3/4 mx-auto" />
+    </>
   );
 }
